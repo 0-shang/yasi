bb754c531b84ecfc28150d3cb5c03cd9f209c033
0-shang <capelasucks@gmail.com>
2025-12-11 16:18:42 +0000

    hh

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = 'ielts-v1';
+const CACHE_NAME = 'vocab-v4';
 const ASSETS = [
-  './index.html',
-  './manifest.json',
-  'https://unpkg.com/vue@3/dist/vue.global.js'
+    './',
+    './index.html',
+    './manifest.json',
+    'https://unpkg.com/vue@3/dist/vue.global.js',
+    'https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
