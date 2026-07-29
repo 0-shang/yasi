7bfecaa0692da60c85d1a470963cf895ff4a9fa9
0-shang <capelasucks@gmail.com>
2026-07-29 13:09:14 +0000

    sd

--- tweets-automator/src/daily_news.js
+++ tweets-automator/src/daily_news.js
@@ -109,11 +109,11 @@ const defaultRssSources = {
     limit: 10,
     shuffle: true,
     sources: [
-      { url: "https://rsshub.rssforever.com/tieba/forum/%E5%BC%B1%E6%99%BA%E5%90%A7", quota: 10, label: "贴吧(弱智吧段子)" },
-      { url: "https://rsshub.rssforever.com/douban/group/explore", quota: 10, label: "豆瓣讨论精选" },
-      { url: "https://rsshub.rssforever.com/chouti/hot", quota: 10, label: "抽屉新热榜" },
-      { url: "https://rsshub.rssforever.com/jike/topic/54dffb40e4b0f57466e675f0", quota: 10, label: "即刻(开发者日常)" },
-      { url: "https://rsshub.rssforever.com/jike/topic/553870e8e4b0eb9b2d69dc06", quota: 10, label: "即刻(即刻精选)" }
+      { url: "https://rsshub.rssforever.com/tieba/forum/%E5%BC%B1%E6%99%BA%E5%90%A7", quota: 10, ignoreSeen: true, label: "贴吧(弱智吧段子)" },
+      { url: "https://rsshub.rssforever.com/douban/group/explore", quota: 10, ignoreSeen: true, label: "豆瓣讨论精选" },
+      { url: "https://rsshub.rssforever.com/chouti/hot", quota: 10, ignoreSeen: true, label: "抽屉新热榜" },
+      { url: "https://rsshub.rssforever.com/jike/topic/54dffb40e4b0f57466e675f0", quota: 10, ignoreSeen: true, label: "即刻(开发者日常)" },
+      { url: "https://rsshub.rssforever.com/jike/topic/553870e8e4b0eb9b2d69dc06", quota: 10, ignoreSeen: true, label: "即刻(即刻精选)" }
     ]
   }
 };
